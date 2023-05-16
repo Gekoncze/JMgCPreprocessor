af58f49bf80f60310f9a4a341149f9afd8dc0eee
Mg <gekoncze@seznam.cz>
2023-05-16 21:35:24 +0200

    added backslash position correction service + test

---
+++ src/cz/mg/c/preprocessor/services/BackslashPositionCorrectionService.java
@@ -0,0 +1,61 @@
+package cz.mg.c.preprocessor.services;
+
+import cz.mg.annotations.classes.Service;
+import cz.mg.annotations.requirement.Mandatory;
+import cz.mg.annotations.requirement.Optional;
+
+public @Service class BackslashPositionCorrectionService {
+    private static @Optional BackslashPositionCorrectionService instance;
+
+    public static @Mandatory BackslashPositionCorrectionService getInstance() {
+        if (instance == null) {
+            instance = new BackslashPositionCorrectionService();
+        }
+        return instance;
+    }
+
+    private BackslashPositionCorrectionService() {
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")
+    public int correct(@Mandatory String content, int position) {
+        validate(content, position);
+
+        int originalPosition = 0;
+        int backslashedPosition = 0;
+
+        while (backslashedPosition < position) {
+            char ch = get(content, originalPosition);
+            char nch = get(content, originalPosition + 1);
+
+            if (ch == '\\' && nch == '\n') {
+                backslashedPosition -= 2;
+            }
+
+            originalPosition++;
+            backslashedPosition++;
+        }
+
+        while (get(content, originalPosition) == '\\' && get(content, originalPosition + 1) == '\n') {
+            originalPosition += 2;
+        }
+
+        return originalPosition;
+    }
+
+    private char get(@Mandatory String content, int i) {
+        if (i < content.length()) {
+            return content.charAt(i);
+        } else {
+            return '\0';
+        }
+    }
+
+    private void validate(@Mandatory String content, int position) {
+        if (position < 0 || position >= content.length()) {
+            throw new ArrayIndexOutOfBoundsException(
+                "Position " + position + " is out of bounds of string of length " + content.length() + "."
+            );
+        }
+    }
+}
