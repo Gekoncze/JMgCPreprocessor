bd1ff9cd457aab2199d28afdb3cd361ad817c0e1
Mg <gekoncze@seznam.cz>
2023-06-04 20:48:02 +0200

    planning didn't realize that "defined" is a system macro

--- src/cz/mg/c/preprocessor/expression/ExpressionProcessor.java
+++ src/cz/mg/c/preprocessor/expression/ExpressionProcessor.java
@@ -3,14 +3,13 @@ package cz.mg.c.preprocessor.expression;
 import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.annotations.requirement.Optional;
+import cz.mg.collections.list.List;
+import cz.mg.collections.list.ListItem;
 import cz.mg.tokenizer.components.TokenReader;
 import cz.mg.tokenizer.entities.Token;
 import cz.mg.tokenizer.entities.tokens.BracketToken;
 import cz.mg.tokenizer.entities.tokens.NumberToken;
 import cz.mg.tokenizer.entities.tokens.OperatorToken;
-import cz.mg.tokenizer.entities.tokens.SpecialToken;
-import cz.mg.collections.list.List;
-import cz.mg.collections.list.ListItem;
 
 public @Service class ExpressionProcessor {
     private static volatile @Service ExpressionProcessor instance;
