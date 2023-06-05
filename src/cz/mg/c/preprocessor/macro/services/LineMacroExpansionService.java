69f1fab25fa890d8eb18589da61ad5e5cf8559e6
Mg <gekoncze@seznam.cz>
2023-06-05 15:57:22 +0200

    corrected line macro position

--- src/cz/mg/c/preprocessor/macro/services/LineMacroExpansionService.java
+++ src/cz/mg/c/preprocessor/macro/services/LineMacroExpansionService.java
@@ -3,6 +3,7 @@ package cz.mg.c.preprocessor.macro.services;
 import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.c.preprocessor.macro.components.MacroExpansion;
+import cz.mg.c.preprocessor.services.BackslashPositionService;
 import cz.mg.collections.list.List;
 import cz.mg.file.File;
 import cz.mg.tokenizer.entities.Token;
@@ -18,6 +19,7 @@ public @Service class LineMacroExpansionService {
                 if (instance == null) {
                     instance = new LineMacroExpansionService();
                     instance.positionService = PositionService.getInstance();
+                    instance.backslashPositionService = BackslashPositionService.getInstance();
                 }
             }
         }
@@ -25,14 +27,16 @@ public @Service class LineMacroExpansionService {
     }
 
     private @Service PositionService positionService;
+    private @Service BackslashPositionService backslashPositionService;
 
     private LineMacroExpansionService() {
     }
 
     public @Mandatory List<Token> expand(@Mandatory MacroExpansion expansion, @Mandatory File file) {
+        int correctedPosition = backslashPositionService.correct(file.getContent(), expansion.getToken().getPosition());
         return new List<>(
             new NumberToken(
-                "" + positionService.find(file.getContent(), expansion.getToken().getPosition()).getRow(),
+                "" + positionService.find(file.getContent(), correctedPosition).getRow(),
                 expansion.getToken().getPosition()
             )
         );
